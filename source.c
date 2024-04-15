408c87d1535918fb37a3d0ecbf3b3e712f0ed8c9
kripticni <157402936+kripticni@users.noreply.github.com>
2024-04-15 22:52:33 +0200

    Optimized version of this algorithm.
    Considering that my first algorithm would have taken forever to finish, the second one about a couple years, the third one around 50 hours... I'm quite satisfied with this one being able to execute on my system in 40 seconds.

--- source.c
+++ source.c
@@ -1,3 +1,6 @@
+
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -66,15 +69,16 @@ int main(){
 			fread(&randValue, sizeof(randValue), 1, randReader);
 			randValue=(randValue%2048)+1000; //securing a 4 digit character
 			fprintf(writer, "%llu", randValue);
+			fprintf(writer, "\n");
 		}
 	}else{
 		writer = fopen("lookup.txt","w");
 		for(i=0;i<2048;i++){
 			for(j=0;j<95;j++){
-				remaining[95]='\0';
-				for(z=0;z<95;z++){
-					remaining[z]='\n';
-				}
+				//remaining[95]='\0';
+				//for(z=0;z<95;z++){
+				//	remaining[z]='\n';
+				//}
 				for(z=0;z<95;z++){
 					remainingSize=95;
 					softreset:
@@ -91,33 +95,35 @@ int main(){
 				for(z=0;z<95;z++){
 					//usleep(10000);
 					fread(&randValue, sizeof(randValue), 1, randReader);
-					for(check=0;check<z;check++){
-						removeChar(remaining,array[j][check],&remainingSize);
-					}
+					//for(check=0;check<z;check++){
+					//removeChar(remaining,array[j][z],&remainingSize);
+					//}
 					remainingIterationSize=remainingSize;
 					for(check=0;check<remainingSize;check++){
-						printf("%c",remaining[check]);
+						//printf("%c",remaining[check]);
 						remainingIteration[check]=remaining[check];
 					}
 					for(check=0;check<j;check++){
                                                 removeChar(remainingIteration,array[check][z],&remainingIterationSize);
                                         }
-					printf("\n%llu\t%i:%i\t%i:%i:%i",randValue, remainingSize, remainingIterationSize, i,j,z);
+					//printf("\n%llu\t%i:%i\t%i:%i:%i",randValue, remainingSize, remainingIterationSize, i,j,z);
 					if(remainingIterationSize!=0){randValue=randValue% remainingIterationSize;}else{randValue=0;}
 					array[j][z]=remaining[randValue];
-				}
-
-			}
-
-			printf("\n\n\n");
-			for(j=0;j<95;j++){
-				for(z=0;z<95;z++){
-					printf("%i:%i\n",j,z);
+					removeChar(remaining,array[j][z],&remainingSize);
 					fprintf(writer, "%c", array[j][z]); //writing the array to file
 				}
+
+			}
+
+			//printf("\n\n\n");
+			//for(j=0;j<95;j++){
+			//	for(z=0;z<95;z++){
+			//		printf("%i:%i:%i\n",i,j,z);
+			//		fprintf(writer, "%c", array[j][z]); //writing the array to file
+			//	}
 				//printf("%i",i);
 				fprintf(writer, "\n");
-			}
+			//}
 		}
 	}
 	fclose(writer);
